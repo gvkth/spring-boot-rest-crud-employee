f4af4b4659b93272f38a223817ab44d8b78a5627
cuongvk <cuongvk.hcm@vnpt.vn>
2023-06-05 23:23:58 +0700

    Service Facade design pattern: cài đặt layer Rest controller: findById

--- src/main/java/com/luv2code/sprinboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/sprinboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.luv2code.sprinboot.cruddemo.entity.Employee;
 import com.luv2code.sprinboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,14 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee==null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
     }
 }
